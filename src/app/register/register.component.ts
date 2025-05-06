1eb8d15768dc027db54bcd0e5f48a5f2a1efb8f9
Ravi <ravi@prodifyllp.com>
2025-05-06 10:25:28 +0530

    Register component UI implementation.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, viewChild, ViewEncapsulation } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router, RouterModule } from '@angular/router';
+
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
+  standalone: true,
   imports: [MatFormFieldModule, MatInputModule, RouterModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent {
+export class RegisterComponent {
 
   constructor(private router: Router){}
 
-
   onSubmit(){
-    this.router.navigate(['/register'])
+    this.router.navigate(['/'])
   }
+
 }
