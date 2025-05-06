421ce24a9c68c02831e9635b4880d4ba80eec674
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-05-06 13:09:38 +0530

    Refactored register component.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,22 +1,38 @@
 import { Component, viewChild, ViewEncapsulation } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
+import { signal } from '@angular/core';
+import {  computed} from '@angular/core';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, RouterModule],
+  imports: [MatFormFieldModule, MatInputModule,MatIconModule, RouterModule , MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent {
 
+  hide = signal(true);
+  step = signal<string>('first');
+  isFirstStep = computed(() => this.step() === 'first');  
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
   constructor(private router: Router){}
 
+  onNavigate(type: string) {
+    this.step.update(() => type);
+  }
   onSubmit(){
-    this.router.navigate(['/'])
+      // this.step.update(() => 'second')
   }
 
 }
