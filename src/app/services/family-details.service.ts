120bccc6555b5a8c79c6771f1a957f7a622e462e
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-05-13 18:07:49 +0530

    main issue fixed for login.

--- src/app/services/family-details.service.ts
+++ src/app/services/family-details.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
 import CreatePerson from '../Models/CreatePerson.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { API } from '../app.settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FamilyDetailsService {
-  clientId: string = sessionStorage.getItem('clientId') as string;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+     
+  }
 
   getAllFamilyMemberDetails() {
-    // let clientId: string = localStorage.getItem('clientId') as string
-
-    return this.http.get(API.getFamilyMemberDetails(this.clientId))
+   
+    let clientId: string = sessionStorage.getItem('clientId') as string;
+    return this.http.get(API.getFamilyMemberDetails(clientId))
   }
   addFamilyDetails(body:any) {
     return this.http.post( API.addFamilyMemberDetails(), body)
   }
   updateFamilyDetails(person_id:string, relation:string){
-    return this.http.put(API.updateFamilyMemberDetails(this.clientId), {person_id ,relationship_type: relation})
+    let clientId: string = sessionStorage.getItem('clientId') as string;
+    return this.http.put(API.updateFamilyMemberDetails(clientId), {person_id ,relationship_type: relation})
 
   }
   deleteFamilyDetails(person_id: string){
-    return this.http.delete(API.deleteFamilyMemberDetails(this.clientId,person_id));
+    let clientId: string = sessionStorage.getItem('clientId') as string;
+    return this.http.delete(API.deleteFamilyMemberDetails(clientId,person_id));
   }
 
 }
