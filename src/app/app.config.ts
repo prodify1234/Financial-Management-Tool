0b388732dfb061510b2e624bb892aa11a9e3f490
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-05-13 16:45:47 +0530

    main updated to haslocation strategy.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.intreceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(
+  providers: [
+    provideHttpClient(
     withInterceptors([authInterceptor])
-  ),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  ),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withHashLocation())]
 };
