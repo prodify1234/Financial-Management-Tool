aef3afaa89a1e799365d1cda515afa9e92121741
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-05-27 11:16:20 +0530

    main packages api issue fixed

--- src/app/components/packages/packages.component.ts
+++ src/app/components/packages/packages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PackageService } from '../../services/package.service';
@@ -12,16 +12,24 @@ import { SnackbarService } from '../../services/snackbar.service';
 })
 export class PackagesComponent {
   packages : any[] =[]
-  constructor( private packageService : PackageService, private snackbarService : SnackbarService ){
+
+
+  /** Dependencies */
+  private packageService = inject(PackageService);
+  private snackbarService = inject(SnackbarService);
+
+  constructor(){
     this.getAllPackages();
   }
 
   getAllPackages(){
-    this.packageService.getAllPackages().subscribe((response:any) => {
-      this.packages = response
-      console.log(response)
-    }, () => {
-      this.snackbarService.error('Internal Server Error!')
+    this.packageService.getAllPackages().subscribe({
+     next : (response : any) => {
+      this.packages = response.data;
+     },
+     error : (error : any)=> {
+      this.snackbarService.error(error.error.details || 'Internal Server Error');
+     }
     })
   }
 
