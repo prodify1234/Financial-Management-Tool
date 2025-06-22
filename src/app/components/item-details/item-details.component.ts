09b9b8ef6263e6c09712907d638611e201f3ef40
Ravi <ravi@prodifyllp.com>
2025-06-22 20:16:06 +0530

    Level 4 table design implementation

--- src/app/components/item-details/item-details.component.ts
+++ src/app/components/item-details/item-details.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { FinancialItemsService } from '../../services/financial-items.service';
+import { MatTableModule } from '@angular/material/table';
+import { TableShimmerComponent } from '../shared/table-shimmer/table-shimmer.component';
 
+
+export interface itemData {
+  sub_classification : string;
+  purchase_value : string;
+  current_value : string;
+  item_type : string;
+  is_depreciating : string;
+  interest_rate : string;
+}
 @Component({
   selector: 'app-item-details',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatTableModule, TableShimmerComponent],
   templateUrl: './item-details.component.html',
   styleUrl: './item-details.component.scss'
 })
@@ -14,7 +25,20 @@ export class ItemDetailsComponent implements OnInit {
 
   constructor(private router : Router, private route:ActivatedRoute, private financialService : FinancialItemsService){}
 
+  allItems : any[] = [];
+  accountSource : any[] = [];
   currentParams:any;
+  loader  = signal<boolean>(false)
+
+  tableDataColumns:string[]=[
+    'sub_classification',
+    'purchase_value',
+    'current_value',
+    'item_type',
+    'is_depreciating',
+    'interest_rate',
+    'expected_roi'
+  ];
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params:any)=>{
@@ -37,8 +61,20 @@ export class ItemDetailsComponent implements OnInit {
 
 
   getItemDetails(body:any){
+    this.loader.update(()=>true);
     this.financialService.getItemDetails(body).subscribe((response:any)=>{
-      console.log('4th Level Response: ', response)
+      console.log('4th Level Response: ', response);
+      this.allItems = response.data.display_name_groups[0].items;
+      this.accountSource = this.allItems.map((item:any)=>({
+        sub_classification : item.sub_classification,
+        purchase_value : item.purchase_value,
+        current_value : item.current_value,
+        item_type : item.item_type,
+        is_depreciating : item.is_depreciating,
+        interest_rate : item.interest_rate,
+        expected_roi : item.expected_roi
+      }))
+      this.loader.update(()=>false);
     })
   }
 
