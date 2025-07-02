31904b7863ce13a3c19a3d64fdc953f8e5014237
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-07-02 15:52:55 +0530

    ng prime muti select added.

--- src/app/services/transaction-details.service.ts
+++ src/app/services/transaction-details.service.ts
@@ -16,7 +16,7 @@ export class TransactionDetailsService {
       page: currentPage,
       page_size: rowsOnPage
     };
-    return this.http.get(API.getTransactionDetails(this.personId), {params})
+    return this.http.get(API.getTransactionDetails(this.auth.personId), {params})
   }
 
 
