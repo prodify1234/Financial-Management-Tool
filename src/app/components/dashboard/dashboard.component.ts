a8252d3b5a87a790c4d872d46d461f30b15cf6b0
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-07-18 10:43:45 +0530

    main Updated few changes in dashboard.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,91 +1,87 @@
-import { Component, OnInit, signal, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  signal,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
 import { CommonModule } from '@angular/common';
 import { SidenavService } from '../../services/sidenav.service';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import Chart from 'chart.js/auto';
 import { DashboardService } from '../../services/dashboard.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { TextTrimPipe } from '../../pipes/text-trim.pipe';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatIconModule, MatListModule, MatButtonModule, HeaderComponent, CommonModule ,MatSidenavModule, MatProgressSpinnerModule],
+  imports: [
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    HeaderComponent,
+    CommonModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    TextTrimPipe,
+    CarouselModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit,AfterViewChecked{
+  
+@ViewChild('dashboard__bank', { static: false }) dashboard!: ElementRef;
+  cardData: any[] = [];
+  spendingData: any[] = [];
+  recurringData: any[] = [];
+  loader: boolean = false;
+  currentIndex = 0;
+  widthMeasured:boolean = false;
+  dashboardWidth = 0;
+  numberofBanks = 0;
+  responsiveOptions = [
+    // {
+    //     // breakpoint: '1400px',
+    //     numVisible: 2,
+    //     numScroll: 1
+    // },
+    // {
+    //     breakpoint: '1199px',
+    //     numVisible: 3,
+    //     numScroll: 1
+    // },
+    // {
+    //     breakpoint: '767px',
+    //     numVisible: 2,
+    //     numScroll: 1
+    // },
+    // {
+    //     breakpoint: '575px',
+    //     numVisible: 1,
+    //     numScroll: 1
+    // }
+]
 
-  // cardData = [
-  // {
-  //   bank: "ICICI Bank",
-  //   totalTransactions: 124,
-  //   totalInflow: 76500,
-  //   totalOutflow: 42150,
-  //   percentage: 55
-  // },
-  // {
-  //   bank: "HDFC Bank",
-  //   totalTransactions: 98,
-  //   totalInflow: 58200,
-  //   totalOutflow: 31200,
-  //   percentage: 44
-  // },
-  // {
-  //   bank: "SBI",
-  //   totalTransactions: 76,
-  //   totalInflow: 43000,
-  //   totalOutflow: 21000,
-  //   percentage: 34
-  // },
-  // {
-  //   bank: "Axis Bank",
-  //   totalTransactions: 54,
-  //   totalInflow: 29500,
-  //   totalOutflow: 15000,
-  //   percentage: 24
-  // },
-  // {
-  //   bank: "Kotak Mahindra",
-  //   totalTransactions: 39,
-  //   totalInflow: 21000,
-  //   totalOutflow: 9000,
-  //   percentage: 17
-  // },
-  // {
-  //   bank: "Yes Bank",
-  //   totalTransactions: 28,
-  //   totalInflow: 15000,
-  //   totalOutflow: 7000,
-  //   percentage: 12
-  // },
-  // {
-  //   bank: "IndusInd Bank",
-  //   totalTransactions: 19,
-  //   totalInflow: 9000,
-  //   totalOutflow: 4000,
-  //   percentage: 8
-  // },
-  // {
-  //   bank: "Punjab National Bank",
-  //   totalTransactions: 12,
-  //   totalInflow: 6000,
-  //   totalOutflow: 2500,
-  //   percentage: 5
-  // }
-  // ];
+  constructor(
+    public sidenavService: SidenavService,
+    private dashboardService: DashboardService
+  ) {
 
-  cardData:any[]=[];
-  spendingData:any[]=[];
-  recurringData:any[]=[];
-  loader:boolean=false;
-
-  constructor(public sidenavService: SidenavService, private dashboardService:DashboardService){}
-
+    
+  }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -93,49 +89,100 @@ export class DashboardComponent implements OnInit {
 
     this.getCashFlow();
     this.getTopSpendingCategories();
-    this.getRecurringTransactions();
+    // this.getRecurringTransactions();
   }
 
-  getCashFlow(){
-    this.loader = true
-    this.dashboardService.getCashFlow().subscribe((response:any)=>{
-      console.log('Cashflow Response: ', response)
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    console.log('Dashboard Width: ', this.dashboard.nativeElement.offsetWidth);
+    this.dashboardWidth = this.dashboard.nativeElement.offsetWidth;
+    this.numberofBanks = Math.floor(this.dashboardWidth / 340);
+    
+    // this.currentIndex = this.numberofBanks
+    
+  }
+
+  // getBanksList(index:number = 0):any[]{
+  //   let len = Math.floor(this.dashboardWidth / 320);
+  //   // return this.cardData.slice(this.currentIndex, this.currentIndex + len);
+
+  // }
+
+  getCashFlow() {
+    this.loader = true;
+    this.dashboardService.getCashFlow().subscribe((response: any) => {
+      console.log('Cashflow Response: ', response);
 
       this.cardData = response.data.banks;
 
       console.log('Card Data: ', this.cardData);
 
       this.loader = false;
-    })
+    });
   }
 
-  getTopSpendingCategories(){
-    this.loader = true
-    this.dashboardService.getTopSpendingCategories().subscribe((response:any)=>{
-      console.log('Spending Categories: ', response)
+  getTopSpendingCategories() {
+    this.loader = true;
+    this.dashboardService
+      .getTopSpendingCategories()
+      .subscribe((response: any) => {
+        console.log('Spending Categories: ', response);
 
-      this.spendingData = response.data.categories;
+        this.spendingData = response.data.categories;
 
-      console.log('Spending Data: ', this.spendingData);
+        console.log('Spending Data: ', this.spendingData);
 
-      this.loader = false;
-    })
+        this.loader = false;
+      });
   }
 
-  getRecurringTransactions(){
-    this.loader = true
-    this.dashboardService.getRecurringTransactions().subscribe((response:any)=>{
-      console.log('Recurring Transactions: ', response);
+  getRecurringTransactions() {
+    this.loader = true;
+    this.dashboardService
+      .getRecurringTransactions()
+      .subscribe((response: any) => {
+        console.log('Recurring Transactions: ', response);
 
-      this.recurringData = response.data.recurring_transactions;
+        this.recurringData = response.data.recurring_transactions;
 
-      console.log('Recurring Data: ', this.recurringData);
+        console.log('Recurring Data: ', this.recurringData);
 
-      this.loader = false;
-    })
+        this.loader = false;
+      });
   }
 
+  getPercent(type: string, card: any): number {
+    if (type === 'inflow') {
+      return (card.inflow / (card.inflow + card.outflow)) * 100;
+    } else {
+      return (card.outflow / (card.inflow + card.outflow)) * 100;
+    }
+  }
 
+  get visibleItems(): any[] {
+    return this.cardData;
+  }
 
+  getCardData(){
+    let data = this.cardData.slice();
+    return data.shift()
+  }
 
+  prev(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  next(): void {
+    if ((this.currentIndex + this.numberofBanks-1) < this.cardData.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  getTranslateX(){
+    let index = this.currentIndex;
+    return `translateX(-${this.currentIndex * 352}px)`;
+  }
 }
