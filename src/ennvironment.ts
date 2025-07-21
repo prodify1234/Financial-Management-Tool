42e1d76c18d630a468d8ab183da3ceb04d72f964
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-07-21 09:49:11 +0530

    main environment is updated to ai-moderator.

--- src/ennvironment.ts
+++ src/ennvironment.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'http://18.218.99.68:81/api/'; // Replace with your actual API URL
+// export const API_URL = 'http://18.218.99.68:81/api/'; // Replace with your actual API URL
+export const API_URL = 'https://ai-moderator.prodifyllp.com/api/';
